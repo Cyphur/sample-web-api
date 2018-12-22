69f097717733f4f39e746d28ce1116c3f6112ce8
= <solodowntv@live.ca>
2018-12-21 23:30:30 -0600

    Initial project commit.

---
+++ Controllers/RestfulController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestfulController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<int> Get(int id)
+        {
+            return id;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
